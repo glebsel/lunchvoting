7d20ce0ac02791e7bbac012e6692325da9d5c846
Глеб Селезнев <Chacha18@yandex.ru>
2020-03-08 07:29:57 +0300

    Logger was added.

--- src/main/java/web/UserServlet.java
+++ src/main/java/web/UserServlet.java
@@ -1,16 +1,17 @@
 package web;
-
+import org.slf4j.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import static org.slf4j.LoggerFactory.getLogger;
 public class UserServlet extends HttpServlet {
-
+    private static final Logger log = getLogger(UserServlet.class);
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        log.debug("redirect to users");
         response.sendRedirect("users.jsp");
     }
 }
