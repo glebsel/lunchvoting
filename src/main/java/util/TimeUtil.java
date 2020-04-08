ac5d981153b1cce860289e27da3ee31587e4dfa5
Глеб Селезнев <Chacha18@yandex.ru>
2020-04-08 16:14:02 +0300

    Start fix all.

--- src/main/java/util/TimeUtil.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-import java.time.LocalDate;
-
-public class TimeUtil {
-    public static boolean isThisDate(LocalDate localDate, LocalDate thisDate) {
-        return localDate.isEqual(thisDate);
-    }
-}
